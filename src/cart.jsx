30f751ea178074fb0d279b95612928f98cc35a0c
Amarachi Joanna Egbuna <egbunajoanna16@gmail.com>
2025-07-17 10:33:08 +0100

    toggle changes

--- src/cart.jsx
+++ src/cart.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { CartContext } from "./cartContext";
 
 const Cart = () => {
@@ -11,64 +11,82 @@ const Cart = () => {
     removeItem,
   } = useContext(CartContext);
 
+  const [isCartVisible, setIsCartVisible] = useState(false);
+  const handleToggle = () => {
+    setIsCartVisible((isOn) => !isOn);
+  };
+
   return (
     <>
-      <div className="cart-box">
-        <h2>Your Cart ({cartItems.length})</h2>
-        <div>
-          {cartItems.map((product) => {
-            return (
-              <div key={product.id} className="cart-product">
-                <div className="cart-product-detail">
-                  <span className="prod-name">{product.name}</span>
-                  <span
-                    className="remove-item"
-                    onClick={() => removeItem(product)}
-                  >
-                    x
-                  </span>
-                </div>
-
-                <div className="cart-quantity">
-                  <span className="prod-price">${product.price}</span>
-                  <div className="flex ">
-                    <button
-                      onClick={() => {
-                        addToCart(product);
-                      }}
-                    >
-                      +
-                    </button>
-                    <p> {product.quantity}</p>
-
-                    <button
-                      onClick={() => {
-                        removeFromCart(product);
-                      }}
-                    >
-                      -
-                    </button>
-                  </div>
-                </div>
-              </div>
-            );
-          })}
-          <hr />
-          <div className="cart-total">
-            {cartItems.length > 0 ? (
-              <>
-                <p>Total: ${cartTotal}</p>
-                <button onClick={() => clearCart()}>Clear Cart</button>
-              </>
-            ) : (
-            
-                <div>
-                  <h3>Cart is empty</h3>
-                <img src="/assets/images/illustration-empty-cart.svg" alt="" />
-                </div>
-            )}
-          </div>
+      <div className="cart-items">
+        <div className="cart-toggle">
+          <h2>Your Cart ({cartItems.length})</h2>
+          <span className="hamburger-menu" onClick={handleToggle}>
+            ☰
+          </span>
         </div>
+
+        {isCartVisible && (
+          <div className={`cart-box ${isCartVisible ? "visible" : "hidden"}`}>
+            <div>
+              {cartItems.map((product) => {
+                return (
+                  <div key={product.id} className="cart-product">
+                    <div className="cart-product-detail">
+                      <span className="prod-name">{product.name}</span>
+                      <span
+                        className="remove-item"
+                        onClick={() => removeItem(product)}
+                      >
+                        x
+                      </span>
+                    </div>
+
+                    <div className="cart-quantity">
+                      <span className="prod-price">${product.price}</span>
+                      <div className="flex ">
+                        <button
+                          onClick={() => {
+                            addToCart(product);
+                          }}
+                        >
+                          +
+                        </button>
+                        <p> {product.quantity}</p>
+
+                        <button
+                          disabled={product.quantity === 1}
+                          onClick={() => {
+                            removeFromCart(product);
+                          }}
+                        >
+                          -
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+              <hr />
+              <div className="cart-total">
+                {cartItems.length > 0 ? (
+                  <>
+                    <p>Total: ${cartTotal}</p>
+                    <button onClick={() => clearCart()}>Clear Cart</button>
+                  </>
+                ) : (
+                  <div>
+                    <h3>Cart is empty</h3>
+                    <img
+                      src="/assets/images/illustration-empty-cart.svg"
+                      alt=""
+                    />
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </>
   );
